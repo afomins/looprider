4c062792edb5f75604e5ecb68eced84b6dffb041
Alex Fomins <afomins@gmail.com>
2018-06-06 09:32:45 +0300

    Add empty libgdx project

---
+++ desktop/src/com/matalok/looprider/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.matalok.looprider.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.matalok.looprider.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
