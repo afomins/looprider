be24fd3f57b4a078bb1a7e595ebeeba5deae7931
Alex Fomins <afomins@gmail.com>
2018-06-27 09:56:28 +0300

    Adding copyright notice

--- desktop/src/com/matalok/looprider/desktop/DesktopLauncher.java
+++ desktop/src/com/matalok/looprider/desktop/DesktopLauncher.java
@@ -1,3 +1,21 @@
+/*
+ * Loop rider
+ * Copyright (C) 2018 Alex Fomins
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
 //------------------------------------------------------------------------------
 package com.matalok.looprider.desktop;
 
@@ -12,9 +30,9 @@ public class DesktopLauncher {
     public static void main (String[] arg) {
         LwjglApplicationConfiguration config = 
           new LwjglApplicationConfiguration();
-        config.width = 480;//320;
-        config.height = 800;//480;
-        config.vSyncEnabled = false;
+        config.width = 320;//480;//320;
+        config.height = 480;//800;//480;
+        config.vSyncEnabled = true;
         config.foregroundFPS = 0;
         config.title = "Loop rider " + Config.version + " | afomins@gmail.com";
         new LwjglApplication(new Main(), config);
